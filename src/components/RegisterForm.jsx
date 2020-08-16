ea2a95caa0ba9dd46f2b43ccea9e05136f953f5c
eduardoaqueiroz <eaq@cin.ufpe.br>
2020-08-16 13:22:10 -0300

    create register form fields

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,19 +1,23 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from 'react';
-import { Input, DatePicker, Select, ConfigProvider, Button } from 'antd';
-import ptBR from 'antd/es/locale/pt_BR';
+import { Input, InputNumber, Select, Button } from 'antd';
 import styled from 'styled-components';
-import 'moment/locale/pt-br';
 
 const { Option } = Select;
 const { TextArea } = Input;
 
-const Form = styled.form``;
+const RegisterWrapper = styled.div``;
 
 const FormGroup = styled.div`
   display: grid;
-  grid-template-columns: 100px 400px;
+  grid-template-columns: 100px 1fr;
   margin: 10px 0;
+`;
+
+const GridGroup = styled.div`
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-gap: 15px;
 `;
 
 const Label = styled.label`
@@ -41,71 +45,79 @@ function RegisterModal({
 }) {
   const [numAuthors, setNumAuthors] = useState(1);
 
+  const getYear = (value) => {
+    if (value && value.length >= 4) {
+      const year = value.substring(value.length - 4);
+      return parseInt(year, 10);
+    }
+    return undefined;
+  };
+
   return (
-    <Form>
-      <ConfigProvider locale={ptBR}>
-        <FormGroup>
-          <Label htmlFor='title'>Título</Label>
-          <Input name='title' defaultValue={title} />
-        </FormGroup>
-        <FormGroup>
-          <Label htmlFor='author1'>Autores</Label>
-          <MultiInputWrapper>
-            <Input name='author1' defaultValue={authors && authors[0]} />
-            <InputWrapper visible={(authors && authors[1]) || numAuthors >= 2}>
-              <Input name='author2' defaultValue={authors && authors[1]} />
-            </InputWrapper>
-            <InputWrapper visible={(authors && authors[2]) || numAuthors >= 3}>
-              <Input name='author3' defaultValue={authors && authors[2]} />
-            </InputWrapper>
-            <InputWrapper visible={(authors && authors[3]) || numAuthors >= 4}>
-              <Input name='author4' defaultValue={authors && authors[3]} />
-            </InputWrapper>
-            <InputWrapper visible={(authors && authors[4]) || numAuthors >= 5}>
-              <Input name='author5' defaultValue={authors && authors[4]} />
-            </InputWrapper>
-            <InputWrapper visible={(authors && authors[5]) || numAuthors >= 6}>
-              <Input name='author6' defaultValue={authors && authors[5]} />
-            </InputWrapper>
-          </MultiInputWrapper>
-          <Button
-            type='dashed'
-            onClick={() => setNumAuthors(numAuthors + 1)}
-            disabled={numAuthors === 6}
-            style={{ marginTop: 5 }}
-          >
-            Adicionar outro autor
-          </Button>
-        </FormGroup>
+    <RegisterWrapper>
+      <FormGroup>
+        <Label htmlFor='title'>Título</Label>
+        <Input name='title' defaultValue={title} />
+      </FormGroup>
+      <FormGroup>
+        <Label htmlFor='author1'>Autores</Label>
+        <MultiInputWrapper>
+          <Input name='author1' defaultValue={authors && authors[0]} />
+          <InputWrapper visible={(authors && authors[1]) || numAuthors >= 2}>
+            <Input name='author2' defaultValue={authors && authors[1]} />
+          </InputWrapper>
+          <InputWrapper visible={(authors && authors[2]) || numAuthors >= 3}>
+            <Input name='author3' defaultValue={authors && authors[2]} />
+          </InputWrapper>
+          <InputWrapper visible={(authors && authors[3]) || numAuthors >= 4}>
+            <Input name='author4' defaultValue={authors && authors[3]} />
+          </InputWrapper>
+          <InputWrapper visible={(authors && authors[4]) || numAuthors >= 5}>
+            <Input name='author5' defaultValue={authors && authors[4]} />
+          </InputWrapper>
+          <InputWrapper visible={(authors && authors[5]) || numAuthors >= 6}>
+            <Input name='author6' defaultValue={authors && authors[5]} />
+          </InputWrapper>
+        </MultiInputWrapper>
+        <Button
+          type='dashed'
+          onClick={() => setNumAuthors(numAuthors + 1)}
+          disabled={numAuthors === 6}
+          style={{ marginTop: 5 }}
+        >
+          Adicionar outro autor
+        </Button>
+      </FormGroup>
+      <GridGroup>
         <FormGroup>
           <Label htmlFor='publisher'>Editora</Label>
           <Input name='publisher' defaultValue={publisher} />
         </FormGroup>
         <FormGroup>
           <Label htmlFor='date'>Publicação</Label>
-          <DatePicker locale='pt-br' defaultValue={publishedDate} />
+          <InputNumber name='date' defaultValue={getYear(publishedDate)} />
         </FormGroup>
-        <FormGroup>
-          <Label htmlFor='date'>Categoria</Label>
-          <Select
-            showSearch
-            placeholder='Selecione uma categoria'
-            optionFilterProp='children'
-            filterOption={(input, option) =>
-              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-            }
-          >
-            <Option value='infantil'>Infatil</Option>
-            <Option value='teen'>Infantil Juvenil</Option>
-            <Option value='tom'>Fábulas</Option>
-          </Select>
-        </FormGroup>
-        <FormGroup>
-          <Label htmlFor='description'>Descrição</Label>
-          <TextArea rows={3} name='description' defaultValue={description} />
-        </FormGroup>
-      </ConfigProvider>
-    </Form>
+      </GridGroup>
+      <FormGroup>
+        <Label htmlFor='date'>Categoria</Label>
+        <Select
+          showSearch
+          placeholder='Selecione uma categoria'
+          optionFilterProp='children'
+          filterOption={(input, option) =>
+            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+          }
+        >
+          <Option value='infantil'>Infatil</Option>
+          <Option value='teen'>Infantil Juvenil</Option>
+          <Option value='tom'>Fábulas</Option>
+        </Select>
+      </FormGroup>
+      <FormGroup>
+        <Label htmlFor='description'>Descrição</Label>
+        <TextArea rows={3} name='description' defaultValue={description} />
+      </FormGroup>
+    </RegisterWrapper>
   );
 }
 
