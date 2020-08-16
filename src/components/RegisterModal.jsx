5102106302a391d37d1154c8b5698d683959f09f
eduardoaqueiroz <eaq@cin.ufpe.br>
2020-08-16 13:22:27 -0300

    upgrade modal

--- src/components/RegisterModal.jsx
+++ src/components/RegisterModal.jsx
@@ -1,11 +1,25 @@
 /* eslint-disable react/prop-types */
 import React, { useState, useEffect } from 'react';
 import { Modal } from 'antd';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
+import { RegisterForm } from '.';
 
 const ModalWrapper = styled.div``;
 
-function RegisterModal({ data }) {
+const ModalContent = styled.div`
+  overflow-y: auto;
+  width: 100%;
+  max-height: 55vh;
+  padding: 24px;
+`;
+
+const CustomizeModal = createGlobalStyle`
+  .ant-modal-body {
+    padding: 0 !important;
+  }
+`;
+
+function RegisterModal({ data, setIsbn }) {
   const [visible, setVisible] = useState(false);
 
   useEffect(() => {
@@ -15,25 +29,37 @@ function RegisterModal({ data }) {
   }, [data]);
 
   const handleOk = () => {
+    setIsbn(null);
     setVisible(false);
   };
 
   const handleCancel = () => {
+    setIsbn(null);
     setVisible(false);
+  };
+
+  const extractData = () => {
+    if (data && data.data && data.data.items && data.data.items.length > 0) {
+      return data.data.items[0].volumeInfo;
+    }
+    return undefined;
   };
 
   return (
     <ModalWrapper>
       <Modal
-        title={data && data.title}
+        title={(data && data.title) || 'Cadastro de livro'}
         visible={visible}
         onOk={handleOk}
         onCancel={handleCancel}
         okText='Cadastrar'
         cancelText='Cancelar'
       >
-        {JSON.stringify(data)}
+        <ModalContent>
+          <RegisterForm {...extractData(data)} />
+        </ModalContent>
       </Modal>
+      <CustomizeModal />
     </ModalWrapper>
   );
 }
